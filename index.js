9f905dfcf4d89e27cb9ae95a40e3af9f3d4e084a
Frot-end Veríssimo <82881439+Dev-Verissimo@users.noreply.github.com>
2021-06-21 15:33:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const nome = prompt( 'Qual é o seu nome ?')
+const nome = prompt( 'Qual é o seu nome ?')
 const pegarTagPorId = document.getElementById('nome')
 pegarTagPorId.innerHTML = `${nome}`
 
